072ff13bb872652d695facfb90d2b25413225a1d
liubai20609 <2918198839@qq.com>
2020-08-25 10:26:48 +0800

    update

--- renderdesign/renderdesign.py
+++ renderdesign/renderdesign.py
@@ -1,5 +1,6 @@
 import csv
 jiaju = ['桌子', '椅子', '地板', '地毯', '墙壁', '橱柜', '床', '电视柜', '书柜', '窗帘', '床被', '靠枕', '电视墙', '天花板', '沙发', '门', '茶几']
+print('Please refer to "connection relation.png" for furniture category')
 print('Please enter the furniture in the scene'+str(jiaju))
 furniture = []
 T = True
@@ -8,7 +9,7 @@ def input_furniture():
     furniture = list(map(str, input().split()))
     for i in furniture:
         if i not in jiaju:
-            print('家具输入错误，请重新输入：')
+            print('Furniture input error, please re-enter：')
             input_furniture()
             break
     return furniture
@@ -32,17 +33,17 @@ for i in range(len(furniture)):
             if k<num:
                 k = num
             continue
-print('渲染灰度图数量为：'+str(k)+'张')
+print('The number of rendered grayscale images is ：'+str(k))
 renderdesign = []
 for i in range(k):
-    theme = '灰度图方案'+ str(i+1) + "=="
+    theme = 'Rendering theme'+ str(i+1) + "=="
     for j in range(len(furniture_index)):
         theme += furniture_numpy[0][furniture_index[j]]+"-"+furniture_numpy[i+2][furniture_index[j]]+"  "
     renderdesign.append(theme)
 
 
 def write_txt(title_name):      #写入txt
-    with open('渲染灰度图方案.txt', 'w') as file_handle:  # .txt可以不自己新建,代码会自动新建
+    with open('Rendering theme.txt', 'w') as file_handle:  # .txt可以不自己新建,代码会自动新建
         for i in range(len(title_name)):
             file_handle.write("{}\n".format(title_name[i]))  # 此时不需在第2行中的转为字符串
         file_handle.close()
